4ca03a64b986658650aefd69cbbfac52efa1bf77
Gemper <95484337+Gemper@users.noreply.github.com>
2022-02-04 19:51:19 +0100

    Modificado
    Lo he vuelto a modificar

--- loops1.java
+++ loops1.java
@@ -1,23 +1,17 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 public class tabla 
 {
 	 public static void main(String[] args) throws IOException 
 	 {
 	    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-	        int N = Integer.parseInt(bufferedReader.readLine().trim());
-	        int i;
-	        for(i=1;i<=10;i++)
-	        {
-	        	System.out.println(N+" x "+i+" = "+(N*i));
-	        }
-	        
-	        bufferedReader.close();
-	    }
-	
-
+	    int N = Integer.parseInt(bufferedReader.readLine().trim());
+	    int i;
+	    for(i=1;i<=10;i++)
+	    {
+	        System.out.println(N+"x"+i+"="+(N*i));
+	    }  
+	    bufferedReader.close();
+	 }
 }
