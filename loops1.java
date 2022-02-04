25fe9117c51815f4b72c8215f233ada743e7b29d
Ismael Morejón Blasco <ismbla01@alu.iesserpis.org>
2022-02-04 19:33:15 +0100

    Arreglado loops1.java

--- loops1.java
+++ loops1.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class loops1 {
     public static void main(String[] args) throws IOException {
@@ -5,10 +8,10 @@ public class loops1 {
 
         int N = Integer.parseInt(bufferedReader.readLine().trim());
         int i;
-        for(i=1;i<=10;i)
-        {
+        for (i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d%n", N, i, i * N);
         }
-        
+
         bufferedReader.close();
     }
 }
